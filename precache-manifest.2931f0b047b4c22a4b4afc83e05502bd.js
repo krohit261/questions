becb6e444914596f96019950ef459052c8800487
skcraaze <skcraaze@gmail.com>
2020-06-10 08:48:49 -0700

    added build files

---
+++ precache-manifest.2931f0b047b4c22a4b4afc83e05502bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdf01493b905ab8ee9423f340752e95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a3f1c042539ff8b5041",
+    "url": "./static/css/main.6aa7076d.chunk.css"
+  },
+  {
+    "revision": "f53be7feaea915c5da64",
+    "url": "./static/js/2.72a774ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.72a774ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3f1c042539ff8b5041",
+    "url": "./static/js/main.0d0d6775.chunk.js"
+  },
+  {
+    "revision": "8291d043b69574f1fec0",
+    "url": "./static/js/runtime-main.f92bfc61.js"
+  }
+]);
